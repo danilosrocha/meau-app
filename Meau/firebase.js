c88ade59865082ce68d33265621e1d4780ac2005
dsrocha <dsrocha@gmail.com>
2022-07-16 20:41:59 -0300

    att

--- Meau/firebase.js
+++ Meau/firebase.js
@@ -6,7 +6,6 @@ import * as firebase from "firebase";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: "AIzaSyCVTSjPALsTUtWaP0WkO_tkCvpck0cbylI",
   authDomain: "meau-app-2798c.firebaseapp.com",
   projectId: "meau-app-2798c",
@@ -14,14 +13,6 @@ const firebaseConfig = {
   messagingSenderId: "959568155326",
   appId: "1:959568155326:web:e175d6899173fb350f44ef",
   measurementId: "G-SF4TFR6XR9"
-=======
-  apiKey: "AIzaSyCAMwZrL0n_8QL2RZV9rrri74EViWCF-oM",
-  authDomain: "meauapp-36487.firebaseapp.com",
-  projectId: "meauapp-36487",
-  storageBucket: "meauapp-36487.appspot.com",
-  messagingSenderId: "786510371209",
-  appId: "1:786510371209:web:faf0595b65dcb24967a958"
->>>>>>> b33dee797fab2f7c0b5b7cde8656ef517544d70a
 };
 
 // Initialize Firebase
