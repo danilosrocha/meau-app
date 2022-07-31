161d6fc5f875330a3528d00d82209aa12b52c84b
dsrocha <dsrocha@gmail.com>
2022-07-31 01:41:34 -0300

    Possivel solucao para imagens dos pets

--- Meau/src/screens/MyPets/index.js
+++ Meau/src/screens/MyPets/index.js
@@ -12,13 +12,15 @@ import {
 
 import { useNavigation } from '@react-navigation/native'
 
-import { auth, db } from '../../../firebase'
+import { auth, db, storage } from '../../../firebase'
 
 import Item from './Item'
 
 export default () => {
 
     const navigation = useNavigation();
+
+
 
     const handleUpdatePet = () => {
         auth
@@ -27,6 +29,8 @@ export default () => {
 
     const idUser = auth.currentUser?.uid;
     const [data, setData] = useState([]);
+    const [petProfilePicture, setPetProfilePicture] = useState();
+
     const getUsers = () => {
         db
             .collection("Pet")
@@ -41,22 +45,35 @@ export default () => {
                             porte: doc.data().porte,
                             especie: doc.data().especie,
                             nome: doc.data().nome,
+                            id: doc.data().id
                         }
+                        console.log("----> User id",user.id)
                         temporyData.push(user)
                     }
                 });
                 setData(temporyData)
             });
     };
+    /*Possivel solucao para imagens*/
+    // useEffect(() => {
+    //     getUsers();
+    //     let imageRef = storage.ref('profilePetPicture/');
+    //     imageRef
+    //         .getDownloadURL()
+    //         .then((url) => {
+    //             setPetProfilePicture(url);
 
-    useEffect(() => {
-        getUsers();
-    }, []);
+    //         })
+    //         .catch((e) => console.log('getting downloadURL of image error => ', e));
+
+    //     console.log("----> Data id",data.id)
+    //     //console.log(imageRef)
+    // }, []);
 
     const renderItem = ({ item }) => (
         <Item item={item} />
     )
-
+    console.log(data)
     return (
         <Container>
 
@@ -65,7 +82,7 @@ export default () => {
                 <FlatList
                     data={data}
                     renderItem={renderItem}
-                    contentContainerStyle={{justifyContent: 'center'}}
+                    contentContainerStyle={{ justifyContent: 'center' }}
                 />
                 <CustomButton onPress={handleUpdatePet}>
                     <CustomButtonText>Adicionar pet</CustomButtonText>
