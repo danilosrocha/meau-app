18a18ab7635183434865392ac7dc859bf74037e9
dsrocha <dsrocha@gmail.com>
2022-08-03 12:20:41 -0300

    Tela de Home Estilo Att

--- Meau/src/screens/Home/Item.js
+++ Meau/src/screens/Home/Item.js
@@ -17,11 +17,16 @@ const TextParam = styled.Text`
   font-size: 20px;
   color: black;
   margin-bottom: 5px;
+  margin-top: 5px;
 `;
 
 const ImagePet = styled.Image`
-  height: 100px;
-  aspect-ratio: 1;
+  height: 200px;
+  width: 300px;
+  margin-top: 20px;
+  border-radius: 30px;
+  
+  /* aspect-ratio: 2; */
 `;
 
 export default ({ item }) => {
@@ -29,10 +34,7 @@ export default ({ item }) => {
     <FlatlistView>
       {!!item.fotoPet && <ImagePet source={{ uri: item.fotoPet }} />}
       <TextParam>Nome: {item.nome}</TextParam>
-      <TextParam>Idade: {item.idade}</TextParam>
-      <TextParam>Especie: {item.especie}</TextParam>
-      <TextParam>Porte: {item.porte}</TextParam>
-      <TextParam>Sexo: {item.sexo}</TextParam>
+
     </FlatlistView>
   );
 };
